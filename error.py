0b9af574775879bb440a08c8a637894238478d78
Jianle <liujl@hawaii.edu>
2024-06-09 23:49:31 -1000

    clean up unused files.

--- error.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication, QPushButton, QMessageBox
-
-class ErrorPopup(QMessageBox):
-    def __init__(self, message, parent=None):
-        super().__init__(parent)
-        self.setIcon(QMessageBox.Critical)
-        self.setText("Error")
-        self.setInformativeText(message)
-        self.setWindowTitle("Error")
